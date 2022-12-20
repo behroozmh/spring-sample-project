64d9e1cf129d58b839074f808bd2072a6116d608
behrooz.mh <behrooz.mh@gmail.com>
2022-12-20 11:36:58 +0330

    behrooz.mh: add Book Entity

---
+++ library/src/main/java/ir/behi/library/entity/Book.java
@@ -0,0 +1,28 @@
+package ir.behi.library.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "Book")
+@Table(name = "Book")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_book")
+    @SequenceGenerator(name = "seq_book", initialValue = 1, allocationSize = 1)
+    private int id;
+    private String name;
+    private int isbn;
+    private Date printDate;
+
+    @ManyToOne
+    private Category category;
+
+
+}

14784463f347907ace3e015d5d7d6a11e1088ee2
behrooz.mh <behrooz.mh@gmail.com>
2022-12-20 11:33:11 +0330

    behrooz.mh: add category Entity

---
+++ library/src/main/java/ir/behi/library/entity/Category.java
@@ -0,0 +1,27 @@
+package ir.behi.library.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_category")
+    @SequenceGenerator(name = "seq_category", initialValue = 1, allocationSize = 1)
+    private int id;
+
+    @NotNull
+    private String name;
+    private Boolean isEnabled;
+
+
+}

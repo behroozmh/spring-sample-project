a3d2b720dc9291ae49e61e83053d1581bc898671
behrooz.mh <behrooz.mh@gmail.com>
2022-12-20 11:41:55 +0330

    behrooz.mh: add Person Entity

---
+++ library/src/main/java/ir/behi/library/entity/Person.java
@@ -0,0 +1,33 @@
+package ir.behi.library.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "Person")
+@Table(name = "Person")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_category")
+    @SequenceGenerator(name = "seq_category", initialValue = 1, allocationSize = 1)
+    private int id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column(name = "national_code", length = 10)
+    private int nationalCode;
+    @Column(name = "id_num", length = 10)
+    private String idNumber;
+
+    @OneToMany(mappedBy = "person")
+    private List<Borrow> borrowList;
+
+}

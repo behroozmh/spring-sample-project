dd2f4e8a9d9a64b330da4dcb1bf355a10ab57884
behrooz.mh <behrooz.mh@gmail.com>
2022-12-24 11:40:19 +0330

    behrooz.mh: fixed change to resolveMessage, ServiceException change RuntimeException to Exception

--- library/src/main/java/ir/behi/library/exception/ServiceException.java
+++ library/src/main/java/ir/behi/library/exception/ServiceException.java
@@ -5,36 +5,38 @@ package ir.behi.library.exception;
  * Date: 12/20/2022
  * TIME: 10:22 AM
  **/
-public class ServiceException extends RuntimeException {
+public class ServiceException extends Exception {
     private String key;
     private String code;
     private Integer status;
 
-    public ServiceException(){
+    public ServiceException() {
 
     }
-    public ServiceException(String message){
+
+    public ServiceException(String message) {
         super(message);
     }
+
     public ServiceException(String message, String key, String code) {
         super(message);
         this.key = key;
         this.code = code;
     }
 
-    public ServiceException(String message, String key, String code,Integer status) {
+    public ServiceException(String message, String key, String code, Integer status) {
         super(message);
         this.key = key;
         this.code = code;
         this.status = status;
     }
 
-    public ServiceException( String key, String code) {
+    public ServiceException(String key, String code) {
         this.key = key;
         this.code = code;
     }
 
-    public ServiceException( String key, String code,Integer status) {
+    public ServiceException(String key, String code, Integer status) {
         this.key = key;
         this.code = code;
         this.status = status;

65f4490f080c7b22995c82e4d79fe88172fd2b6e
behrooz.mh <behrooz.mh@gmail.com>
2022-12-25 11:22:38 +0330

    behrooz.mh: change test method

--- library/src/test/java/ir/behi/library/LibraryApplicationTests.java
+++ library/src/test/java/ir/behi/library/LibraryApplicationTests.java
@@ -1,37 +1,34 @@
 package ir.behi.library;
 
 import ir.behi.library.dto.CategoryDTO;
-import ir.behi.library.dto.PersonDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.Assert;
 
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LibraryApplicationTests {
-    @Value(value="${local.server.port}")
-    private int port=8081;
-    @Value(value="${server.servlet.context-path}")
-    private String context="library";
+    @Value(value = "${local.server.port}")
+    private int port = 8081;
+    @Value(value = "${server.servlet.context-path}")
+    private String context = "library";
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     void contextLoadsTestWithRestTemplate() {
-        List<CategoryDTO> lstCategory= this.restTemplate.getForObject("http://localhost:" + port +context+ "/category/getAll",
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setName("Test");
+        String result = this.restTemplate.postForObject("http://localhost:" + port + context + "/category/add",
+                categoryDTO, String.class);
+        Assert.hasText(result, "category.is.exist");
+        //
+        List<CategoryDTO> lstCategory = this.restTemplate.getForObject("http://localhost:" + port + context + "/category/getAll",
                 List.class);
-        Assert.notEmpty(lstCategory,"Category is Empty");
-
-        List<PersonDTO> lstPerson= this.restTemplate.getForObject("http://localhost:" + port +context+ "/person/getAll",
-                List.class);
-        Assert.notEmpty(lstPerson,"Person is Empty");
+        Assert.notEmpty(lstCategory, "Category is Empty");
     }
-
-
-
 }

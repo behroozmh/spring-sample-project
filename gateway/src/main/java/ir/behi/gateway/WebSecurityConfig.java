54955b809809ff8b2bc2aa7dd2f4053d74e0dc91
behrooz.mh <behrooz.mh@gmail.com>
2023-02-01 19:01:26 +0330

    behrooz.mh: add pkce for webSecurity filterChange

--- gateway/src/main/java/ir/behi/gateway/WebSecurityConfig.java
+++ gateway/src/main/java/ir/behi/gateway/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package ir.behi.gateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers;
 import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebSecurity
 //@EnableWebFluxSecurity
@@ -29,6 +31,14 @@ public class WebSecurityConfig {
         DefaultServerOAuth2AuthorizationRequestResolver resolver = new DefaultServerOAuth2AuthorizationRequestResolver(repo);
         resolver.setAuthorizationRequestCustomizer(OAuth2AuthorizationRequestCustomizers.withPkce());
         return resolver;
+    }
+
+    @Bean
+    public SecurityWebFilterChain pkceFilterChain(ServerHttpSecurity http,
+                                                  ServerOAuth2AuthorizationRequestResolver resolver) {
+        http.authorizeExchange(r -> r.anyExchange().authenticated());
+        http.oauth2Login(auth -> auth.authorizationRequestResolver(resolver));
+        return http.build();
     }
 
 }

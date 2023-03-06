ce37e8444d32e258dc99989ea10e045da7094fe7
behrooz.mh <behroozmh@gmail.com>
2023-03-06 19:46:29 +0330

    behooz.mh: add new items for dto and rest controller

---
+++ session-with-redis/src/test/java/ir/behi/session_with_redis/SessionWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.behi.session_with_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionWithRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

353b1fbe8a7e774f0ecd525c5ac2660e8b59a989
behrooz.mh <behroozmh@gmail.com>
2023-04-24 16:18:12 +0330

    behooz.mh: add dependency for config for enabled redis for custom bean

--- session-with-redis/src/main/java/ir/behi/session_with_redis/config/RedisConfig.java
+++ session-with-redis/src/main/java/ir/behi/session_with_redis/config/RedisConfig.java
@@ -1,8 +1,17 @@
 package ir.behi.session_with_redis.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 public class RedisConfig {
-
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName("192.168.10.30");
+        factory.setPort(6379);
+        factory.setPassword("1qaz!QAZ");
+        return factory;
+    }
 }

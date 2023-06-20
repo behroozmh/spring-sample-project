4d28d222958a33617e22074ab2456370616c9a2e
behrooz.mh <behroozmh@gmail.com>
2023-06-20 16:22:49 +0330

    behrooz.mh: add yaml

---
+++ YmlAtRuntime/src/test/java/ir/behi/YmlAtRuntime/YmlAtRuntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.behi.YmlAtRuntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmlAtRuntimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
